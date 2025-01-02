78d5672f71a8e20ff39546badccc77029a67a26f
ikiovi <52862362+ikiovi@users.noreply.github.com>
2025-01-02 16:15:30 +0300

    Mark `IChangedTracks.HasChanges` with the `JsonIgnore` attribute.

--- src/SpotifyPlaylistQueryMod.Shared/API/IChangedTracks.cs
+++ src/SpotifyPlaylistQueryMod.Shared/API/IChangedTracks.cs
@@ -1,8 +1,11 @@
-﻿namespace SpotifyPlaylistQueryMod.Shared.API;
+﻿using System.Text.Json.Serialization;
+
+namespace SpotifyPlaylistQueryMod.Shared.API;
 
 public interface IChangedTracks<out T> where T : IBasicTrackInfo
 {
     public IReadOnlyList<T> Added { get; }
     public IReadOnlyList<T> Removed { get; }
+    [JsonIgnore]
     public bool HasChanges => Added.Count + Removed.Count > 0;
 }
